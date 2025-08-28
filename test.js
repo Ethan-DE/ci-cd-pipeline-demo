d95a23db81cfdf15084223ddb8fefc791d699d13
Ethan-DE <eradirideitie@gmail.com>
2025-08-28 22:10:00 +0300

    Remove duplicate root test (CI uses app/test.js)

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-// tiny test for CI
-const http = require("http");
-const req = http.request({ host: "localhost", port: 3000, path: "/", method: "GET" }, res => {
-  process.exit(res.statusCode === 200 ? 0 : 1);
-});
-req.on("error", () => process.exit(1));
-req.end();
+
